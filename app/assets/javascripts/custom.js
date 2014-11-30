a57a47e35db96bb5d9add9ad0a90f1168f455ee3
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-11-30 00:38:01 +0000

    structure

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,107 @@
+//LOADING PAGE//
+$(window).load(function() {
+	$('.load-bg').animate({"width":"250%", "padding-top":"250%"});
+	setTimeout(function(){
+       $('.load-bg').fadeOut();
+    },2000);
+});
+
+
+
+
+$(document).ready(function() {
+	
+	$('body').smoothWheel()
+
+	$('.fish-link').click(function (e) {
+	    e.preventDefault();// prevent default anchor behavior
+	    var goTo = this.getAttribute("href"); // store anchor href
+	    $(".load-bg").show();
+	    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+	    // do something while timeOut ticks ...
+	    setTimeout(function(){
+	       window.location = goTo;
+	    },2500);
+	});
+
+	$('form').submit(function(){
+		setTimeout(function() {
+	    	$('form').submit();
+	  	},3500);
+		$(".load-bg").show();
+	    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+	   	$(".submit").addClass('submit-move');
+	});
+
+	if (window.matchMedia("(max-width: 550px)").matches) {
+	    $('.logo-link').click(function (e) {
+	    	$(this).removeAttr("href");
+		    e.preventDefault();// prevent default anchor behavior
+		    $(".close-button-index").show();
+		    $(".menu-mobile").show();
+		    $('.menu-mobile').animate({"width":"75%", "padding-top":"75%", "border-radius":"50%"}, 2500);
+		    // store anchor href
+		    $(".load-bg").show();
+		    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+		});
+		$('.menu-link').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    var goTo = this.getAttribute("href"); // store anchor href
+		    $('.menu-mobile').animate({"width":"0%", "padding-top":"0%"},1000);
+		    // do something while timeOut ticks ...
+		    setTimeout(function(){
+		       window.location = goTo;
+		    }, 1000);
+		});
+		$('.portfolio-link').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    var goTo = this.getAttribute("href"); // store anchor href
+		    $(".load-bg").show();
+		    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+		    // do something while timeOut ticks ...
+		    setTimeout(function(){
+		       window.location = goTo;
+		    },2500);
+		});
+		$('.close-button-index').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    $(this).fadeOut();
+		    $('.menu-mobile').animate({"width":"0%", "padding-top":"0%"},1000);
+		    setTimeout(function(){
+		       $('.menu-mobile').hide();
+		    },1000);
+		    // do something while timeOut ticks ...
+		    $('.load-bg').animate({"width":"250%", "padding-top":"250%"});
+	    	setTimeout(function(){
+		       $('.load-bg').hide();
+		    },2000);
+		});
+	  }
+	else {
+	   $('.logo-link').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    var goTo = this.getAttribute("href"); // store anchor href
+		    $(".load-bg").show();
+		    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+		    // do something while timeOut ticks ...
+		    setTimeout(function(){
+		       window.location = goTo;
+		    },2500);
+		});
+	 }
+
+
+
+
+	$('.logo-contact-link').click(function (e) {
+	    e.preventDefault();// prevent default anchor behavior
+	    var goTo = this.getAttribute("href"); // store anchor href
+	    $(".load-bg").show();
+	    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+	   
+	    // do something while timeOut ticks ...
+	    setTimeout(function(){
+	       window.location = goTo;
+	    },2500);
+	});
+});
---
+++ app/assets/javascripts/portfolio.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	if (window.matchMedia("(min-width: 920px)").matches) {
+	  $('.illuminator').css({"width":"50%", "padding-top":"50%"});
+	} else {
+	  $('.illuminator').css({"width":"90%", "padding-top":"90%"});
+	}
+	$('.a-close-button').click(function (e) {
+	    e.preventDefault();                   // prevent default anchor behavior
+	    $("this").fadeOut();
+	    var goTo = this.getAttribute("href"); // store anchor href
+
+	    // do something while timeOut ticks ...
+
+	    $(".illuminator").css({"width":"0%", "padding-top":"0%"});
+	    $(".share").css({"opacity":"0"});
+	    setTimeout(function(){
+	       window.location = goTo;
+	    },1200);
+	}); 
+});
+
+$(window).scroll(function() {
+    if ($(document).height() - 150 <= ($(window).height() + $(window).scrollTop())) {
+        $(".share").addClass("scroll-bottom");
+    }
+    else {
+    	$(".share").removeClass("scroll-bottom");
+    }
+});
