7d9074b3b8320ec76524b679b822eae5a24da5ee
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-11-30 15:06:11 +0000

    css

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	$('.illuminator').animate({"border":"5000px solid #000"});
 	if (window.matchMedia("(min-width: 920px)").matches) {
-	  $('.illuminator').animate({"width":"50%", "padding-top":"50%"},2500);
+	  $('.illuminator').animate({"width":"50%", "padding-top":"50%"},2000);
 	} else {
-	  $('.illuminator').animate({"width":"90%", "padding-top":"90%"},2500);
+	  $('.illuminator').animate({"width":"90%", "padding-top":"90%"},2000);
 	}
 	$('.a-close-button').click(function (e) {
 	    e.preventDefault();                   // prevent default anchor behavior
@@ -12,11 +12,11 @@ $(document).ready(function(){
 
 	    // do something while timeOut ticks ...
 
-	    $(".illuminator").animate({"width":"0%", "padding-top":"0%"},2500);
+	    $(".illuminator").animate({"width":"0%", "padding-top":"0%"},2000);
 	    $(".share").css({"opacity":"0"});
 	    setTimeout(function(){
 	       window.location = goTo;
-	    },2500);
+	    },3500);
 	}); 
 });
 
