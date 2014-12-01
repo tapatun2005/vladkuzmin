a3a9c5b5c427523be756f0ac4627d3251821581b
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-01 00:39:59 +0000

    hello

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-   config.assets.precompile += %w( application-ie.css )
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
