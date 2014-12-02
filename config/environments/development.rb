a586d04ae40643087153afb0bf8e587c5a693bee
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-02 10:21:53 +0000

    css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  
-  config.assets.precompile += %w( application-ie.css )
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
