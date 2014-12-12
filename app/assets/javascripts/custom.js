b872f4c4b6c14315f492ad65e49898392a0746db
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-12 21:56:05 +0000

    background fix

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,6 @@
 //LOADING PAGE//
 $(window).load(function() {
+	$('body, html').css({"background":"none"})
 	$('.load-bg').animate({"width":"250%", "padding-top":"250%"});
 	setTimeout(function(){
        $('.load-bg').fadeOut();
