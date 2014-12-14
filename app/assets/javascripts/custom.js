fceeef1640603a963937fb5d61656e5fccc651f0
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-14 13:40:51 +0000

    js

--- app/assets/javascripts/custom.js
+++
@@ -1,7 +0,0 @@
-$(window).load(function() {
-	$('body, html').css({"background":"none"})
-	$('.load-bg').animate({"width":"250%", "padding-top":"250%"});
-	setTimeout(function(){
-       $('.load-bg').fadeOut();
-    },2000);
-});
---
+++ public/js/custom.js
@@ -0,0 +1,19 @@
+//LOADING PAGE//
+$(document).ready(function() {
+	$('body, html').css({"background":"none"})
+	$('.load-bg').animate({"width":"250%", "padding-top":"250%"});
+	setTimeout(function(){
+       $('.load-bg').fadeOut();
+    },2000);
+
+    $('a').click(function (e) {
+	    e.preventDefault();// prevent default anchor behavior
+	    var goTo = this.getAttribute("href"); // store anchor href
+	    $(".load-bg").show();
+	    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+	    // do something while timeOut ticks ...
+	    setTimeout(function(){
+	       window.location = goTo;
+	    },3500);
+	});
+});
