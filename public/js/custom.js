4c10f026d887bba18c5a8e4b0c346b2f504cf566
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-15 22:26:41 +0000

    load bg

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
        $('.load-bg').fadeOut();
     },2000);
 
-    $('a, .fish-link').click(function (e) {
+    $('.fish-link').click(function (e) {
 	    e.preventDefault();// prevent default anchor behavior
 	    var goTo = this.getAttribute("href"); // store anchor href
 	    $(".load-bg").show();
@@ -36,6 +36,80 @@ $(document).ready(function() {
 	    setTimeout(function(){
 	       window.location = goTo;
 	    },3500);
-	}); 
+	});
+
+
+
+
+//MENU//
+
+
+	if (window.matchMedia('(max-width: 550px)').matches) {
+	    $('.logo-link').click(function (e) {
+	    	$(this).removeAttr("href");
+		    e.preventDefault();// prevent default anchor behavior
+		    $(".close-button-index").show();
+		    $(".menu-mobile").show();
+		    $('.menu-mobile').animate({"width":"75%", "padding-top":"75%", "border-radius":"50%"}, 2500);
+		    // store anchor href
+		    $(".load-bg").fadeIn();
+		    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+		});
+		$('.menu-link').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    $('.close-button-index').fadeOut();
+		    var goTo = this.getAttribute("href"); // store anchor href
+		    $('.menu-mobile').animate({"width":"0%", "padding-top":"0%"},1000);
+		    // do something while timeOut ticks ...
+		    setTimeout(function(){
+		       window.location = goTo;
+		    }, 1000);
+		});
+		$('.portfolio-link').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    var goTo = this.getAttribute("href"); // store anchor href
+		    $(".load-bg").show();
+		    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+		    // do something while timeOut ticks ...
+		    setTimeout(function(){
+		       window.location = goTo;
+		    },3500);
+		});
+		$('.close-button-index').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    $(this).fadeOut();
+		    $('.menu-mobile').animate({"width":"0%", "padding-top":"0%"},1000);
+		    setTimeout(function(){
+		       $('.menu-mobile').hide();
+		    },1000);
+		    // do something while timeOut ticks ...
+		    $('.load-bg').animate({"width":"250%", "padding-top":"250%"});
+	    	setTimeout(function(){
+		       $('.load-bg').hide();
+		    },2000);
+		});
+	  }
+	else {
+	   $('.logo-link').click(function (e) {
+		    e.preventDefault();// prevent default anchor behavior
+		    var goTo = this.getAttribute("href"); // store anchor href
+		    $(".load-bg").show();
+		    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+		    // do something while timeOut ticks ...
+		    setTimeout(function(){
+		       window.location = goTo;
+		    },3500);
+		});
+	 }
+
+//FORM//
+$('form').submit(function(){
+		setTimeout(function() {
+	    	$('form').submit();
+	  	},3500);
+		$(".load-bg").show();
+	    $('.load-bg').animate({"width":"0%", "padding-top":"0%"});
+	   	$(".submit").addClass('submit-move');
+	});
 
 });
