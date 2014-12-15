6b30e6407818e5200a19686f7712c4e8298006a4
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-15 21:51:21 +0000

    load bg

--- public/js/custom.js
+++ public/js/custom.js
@@ -21,18 +21,16 @@ $(document).ready(function() {
 
 //PORTFOLIO PAGE//
 	if (window.matchMedia("(min-width: 920px)").matches) {
-		  $('.illuminator').css({"width":"50%", "padding-top":"50%"},2000);
+		  $('.illuminator').animate({"width":"50%", "padding-top":"50%"},2000);
 		} 
 		else {
-		  $('.illuminator').css({"width":"90%", "padding-top":"90%"},2000);
+		  $('.illuminator').animate({"width":"90%", "padding-top":"90%"},2000);
 	}
 	$('.a-close-button').click(function (e) {
 	    e.preventDefault();                   // prevent default anchor behavior
 	    $(this).fadeOut();
 	    var goTo = this.getAttribute("href"); // store anchor href
-
 	    // do something while timeOut ticks ...
-
 	    $(".illuminator").css({"width":"0%", "padding-top":"0%"},2000);
 	    $(".share").css({"opacity":"0"});
 	    setTimeout(function(){
