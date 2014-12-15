e54a99cb1e75aec390ac9ea56d4efa90a1d31ff2
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-15 21:05:42 +0000

    load bg

--- public/js/custom.js
+++ public/js/custom.js
@@ -18,4 +18,27 @@ $(document).ready(function() {
 	    },3500);
 	});
 
+
+
+	$('.illuminator').animate({"border":"5000px solid #000"});
+	if (window.matchMedia("(min-width: 920px)").matches) {
+	  $('.illuminator').animate({"width":"50%", "padding-top":"50%"},2000);
+	} 
+	else {
+	  $('.illuminator').animate({"width":"90%", "padding-top":"90%"},2000);
+	}
+	$('.a-close-button').click(function (e) {
+	    e.preventDefault();                   // prevent default anchor behavior
+	    $(this).fadeOut();
+	    var goTo = this.getAttribute("href"); // store anchor href
+
+	    // do something while timeOut ticks ...
+
+	    $(".illuminator").animate({"width":"0%", "padding-top":"0%"},2000);
+	    $(".share").css({"opacity":"0"});
+	    setTimeout(function(){
+	       window.location = goTo;
+	    },3500);
+	}); 
+
 });
