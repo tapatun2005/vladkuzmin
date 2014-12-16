ada968e93ddd4e0066904f5f3f7790f6c6dd483d
Vlad Kuzmin <vlad.kuzmin@blueclaw.co.uk>
2014-12-16 12:28:08 +0000

    js

--- public/js/custom.js
+++ public/js/custom.js
@@ -20,25 +20,23 @@ $(document).ready(function() {
 
 
 //PORTFOLIO PAGE//
-	if (window.matchMedia("(min-width: 920px)").matches) {
-		  $('.illuminator').animate({"width":"50%", "padding-top":"50%"},2000);
-		} 
-		else {
-		  $('.illuminator').animate({"width":"90%", "padding-top":"90%"},2000);
-	}
-	$('.a-close-button').click(function (e) {
-	    e.preventDefault();                   // prevent default anchor behavior
-	    $(this).fadeOut();
-	    var goTo = this.getAttribute("href"); // store anchor href
-	    // do something while timeOut ticks ...
-	    $(".illuminator").animate({"width":"0%", "padding-top":"0%"},2000);
-	    $(".share").animate({"opacity":"0"});
-	    setTimeout(function(){
-	       window.location = goTo;
-	    },3500);
-	});
-
-
+//	if (window.matchMedia("(min-width: 920px)").matches) {
+//		  $('.illuminator').animate({"width":"50%", "padding-top":"50%"},2000);
+//		} 
+//		else {
+//		  $('.illuminator').animate({"width":"90%", "padding-top":"90%"},2000);
+//	}
+//	$('.a-close-button').click(function (e) {
+//	    e.preventDefault();                   // prevent default anchor behavior
+//	    $(this).fadeOut();
+//	    var goTo = this.getAttribute("href"); // store anchor href
+//	    // do something while timeOut ticks ...
+//	    $(".illuminator").animate({"width":"0%", "padding-top":"0%"},2000);
+//	    $(".share").animate({"opacity":"0"});
+//	    setTimeout(function(){
+//	       window.location = goTo;
+//	    },3500);
+//	});
 
 
 //MENU//
